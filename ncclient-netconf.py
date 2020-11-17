0fb8f0fbbeb0f76f62941ede634bf237f9c54f58
Gouranga Jena <64470929+sugo-85@users.noreply.github.com>
2020-11-17 10:21:06 +0000

    Add files via upload

---
+++ ncclient-netconf.py
@@ -0,0 +1,66 @@
+from ncclient import manager
+import xml.dom.minidom
+
+m = manager.connect(
+    host="192.168.56.3",
+    port=830,
+    username="cisco",
+    password="cisco123!",
+    hostkey_verify=False
+)
+'''
+print("#Supported Capabilities (YANG models):")
+for capability in m.server_capabilities:
+    print(capability)
+
+netconf_reply = m.get_config(source="running")
+print(netconf_reply)
+
+print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
+
+netconf_filter = """
+<filter>
+<native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native" />
+</filter>
+"""
+netconf_reply = m.get_config(source="running", filter=netconf_filter)
+print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
+
+'''
+
+netconf_hostname = """
+<config>
+<native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+<hostname>CSR1kv</hostname>
+</native>
+</config>
+"""
+
+netconf_reply = m.edit_config(target="running", config=netconf_hostname)
+print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
+
+
+netconf_loopback = """
+<config>
+<native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+<interface>
+<Loopback>
+<name>1</name>
+<description>My first NETCONF loopback</description>
+<ip>
+<address>
+<primary>
+<address>10.1.1.1</address>
+<mask>255.255.255.0</mask>
+</primary>
+</address>
+</ip>
+</Loopback>
+</interface>
+</native>
+</config>
+"""
+
+netconf_reply = m.edit_config(target="running", config=netconf_loopback)
+print(xml.dom.minidom.parseString(netconf_reply.xml).toprettyxml())
+
